601046fdd81a188b0b0fe4e7d0127f9740c3b777
Conor Barthel <conor.barthel@gmail.com>
2022-06-07 16:23:13 -0600

    Feat: factories for subscription tea and customer

---
+++ spec/factories/customer.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :customer do
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    address { Faker::Address.street_address }
+    zipcode { Faker::Address.zip }
+    state { Faker::Address.state }
+  end
+end
