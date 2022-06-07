04d49e721fae3d0948bb07f909b4e5e32ccbdfbb
Conor Barthel <conor.barthel@gmail.com>
2022-06-07 09:46:25 -0600

    Feat: Create database

---
+++ db/migrate/20220607153226_create_customers.rb
@@ -0,0 +1,14 @@
+class CreateCustomers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :customers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :address
+      t.string :zipcode
+      t.string :state
+
+      t.timestamps
+    end
+  end
+end
